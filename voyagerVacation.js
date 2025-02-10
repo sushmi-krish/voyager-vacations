5376ba6eaae05a6873ca1de8db83b4e55421f40f
sushmi-krish <sushmithakrishnamoorthy@gmail.com>
2025-02-09 21:49:12 -0500

    modified files

--- voyagerVacation.js
+++ voyagerVacation.js
@@ -2,7 +2,7 @@
 //.then(response =>{return response.json()})
 //.then(temple => console.log(temple.temples))
 //.then(data =>{ console.log(data.beaches)})
-const input = document.getElementById('input').value.toLowerCase();
+const input = document.getElementById('input')
 const btnOne = document.getElementById('buttonOne');
 const btnTwo = document.getElementById('buttonTwo');
 const result =document.getElementById('result')
@@ -10,9 +10,9 @@ btnOne.addEventListener('click',()=>{
     fetch('travel_recommendation_api.json')
 .then(response =>{return response.json()})
 .then(data =>{
-    console.log(data)
- if(data.countries.cities){
+
   const country = data.countries.find(item => item.name.toLowerCase() === input.value.toLowerCase())
+ if(country){
    console.log(country);
    const cities = country.cities;
     console.log(cities)
@@ -30,22 +30,61 @@ btnOne.addEventListener('click',()=>{
         p.innerHTML =`${city.description}`
         div.appendChild(p)
         result.append(div)
-        
+        input.value = '';
     })
+  }
+else if(input.value.toLowerCase() ==='temples'){
+    let temples =data.temples;
+   temples.forEach(temple =>{
+    let div = document.createElement('div')
+    let img = document.createElement('img')
+    img.src =`${temple.imageUrl}`
+    div.appendChild(img)
+
+    let h1 =document.createElement('h1')
+    h1.innerHTML =`${temple.name}`;
+    div.appendChild(h1)
+
+    let p =document.createElement('p');
+    p.innerHTML=`${temple.description}`;
+    div.appendChild(p)
+    result.append(div)
+    input.value='';
+   })
 }
-   else if( data.temples){
-   console.log(data.temples.find(item => item.name.toLowerCase() === input.value.toLowerCase()))
-    console.log(temple)
-   }
-   else{
-    const beach = data.beaches.find(item => item.name.toLowerCase() === input.value.toLowerCase())
-   console.log(beach)
-   }
+else if(input.value.toLowerCase() ==='beaches'){
+    let beaches = data.beaches;
+   
+    beaches.forEach(beach =>{
+     let div = document.createElement('div')
+     let img = document.createElement('img')
+     img.src =`${beach.imageUrl}`
+     div.appendChild(img)
+ 
+     let h1 =document.createElement('h1')
+     h1.innerHTML =`${beach.name}`;
+     div.appendChild(h1)
+ 
+     let p =document.createElement('p');
+     p.innerHTML=`${beach.description}`;
+     div.appendChild(p)
+     result.append(div)
+     input.value ='';
+    })
+    
+}
+else {
+    result.innerHTML = '<p>No matching results found. Please try again.</p>';
+  }
 })
+.catch(error => {
+    console.error('Error fetching the data:', error);
+    result.innerHTML = `<p>Error fetching data. Please try again later.</p>`;
+    input.value=''
+  });
 
-.catch(error =>{
-    console.log(error)
-   let h1 = document.createElement('h1')
-   h1.innerHTML = error;
 })
-
+btnTwo.addEventListener('click',()=>{
+    result.style.display='block';
+    result.innerHTML='';
+})
